14c87c6e5d4c32d712bbe1c0f555d0a57d36e4ad
Raymond Li <li.raymond04@gmail.com>
2024-06-06 00:06:36 -0600

    Test hello api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
+import Button from "@/components/Navbar/Button";
+
 const Home = () => {
   return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      Test body
-    </div>
+    <>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        Test body
+      </div>
+      <div className="flex">
+        <Button
+          onClick={async () => {
+            const data = await fetch("/api/hello")
+            const json = await data.json()
+            console.log(json)
+          }}
+        >
+          Get data
+        </Button>
+      </div>
+    </>
   );
 }
 
