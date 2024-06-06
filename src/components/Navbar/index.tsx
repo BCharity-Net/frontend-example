eb96894f71d0ffebc51adddcab07d53dafb2153b
Raymond Li <li.raymond04@gmail.com>
2024-06-05 23:28:36 -0600

    Barebones project

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Navbar: FC = () => {
+  return (
+    <div className="bg-slate-800 p-4 rounded-md flex space-x-20 items-center">
+      Hello Navbar
+    </div>
+  );
+};
+
+export default Navbar;
